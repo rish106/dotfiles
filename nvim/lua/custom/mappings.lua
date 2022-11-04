f740a303d403a76ade3e7ec0d785da9cc91b5a04
Rishabh Verma <10rishabhv@gmail.com>
2022-11-04 15:08:57 +0530

    change mapping to run files with space in their names

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -12,15 +12,19 @@ M.disabled = {
 		["<C-c>"] = "",
 		["<C-b>"] = "",
 		["<C-e>"] = "",
-		["<C-h>"] = "",
 		["<C-j>"] = "",
 		["<C-k>"] = "",
-		["<C-l>"] = "",
 		["K"] = "",
 		["<TAB>"] = "",
 		["<S-Tab>"] = "",
 		["<leader>y"] = "",
 	},
+    i = {
+		["<C-h>"] = "",
+		["<C-j>"] = "",
+		["<C-k>"] = "",
+		["<C-l>"] = "",
+    }
 }
 
 M.general = {
@@ -31,6 +35,7 @@ M.general = {
 	n = {
 		-- Copy all
 		["YY"] = { "<cmd> %y* <CR>", "copy whole file" },
+        ["<leader>fw"] = {"@g"},
 	},
 	x = {
 		-- copy selected lines to clipboard
@@ -95,64 +100,12 @@ M.cphelper = {
 }
 
 vim.cmd[[
-" for detecting OS
-if !exists("g:os")
-	if has("win64") || has("win32") || has("win16")
-		let g:os = "Windows"
-	else
-		let g:os = substitute(system('uname'), '\n', '', '')
-	endif
-endif
-
-" important option that should already be set!
-set hidden
-
-" available options:
-" * g:split_term_style
-" * g:split_term_resize_cmd
-function! TermWrapper(command) abort
-	if !exists('g:split_term_style') | let g:split_term_style = 'vertical' | endif
-	if g:split_term_style ==# 'vertical'
-		let buffercmd = 'vnew'
-	elseif g:split_term_style ==# 'horizontal'
-		let buffercmd = 'new'
-	else
-		echoerr 'ERROR! g:split_term_style is not a valid value (must be ''horizontal'' or ''vertical'' but is currently set to ''' . g:split_term_style . ''')'
-		throw 'ERROR! g:split_term_style is not a valid value (must be ''horizontal'' or ''vertical'')'
-	endif
-	exec buffercmd
-	if exists('g:split_term_resize_cmd')
-		exec g:split_term_resize_cmd
-	endif
-	exec 'term ' . a:command
-	exec 'setlocal nornu nonu'
-	exec 'startinsert'
-	autocmd BufEnter <buffer> startinsert
-endfunction
-
-command! -nargs=0 CompileAndRunCpp call TermWrapper(printf('g++ -std=c++17 %s && ./a.out', expand('%')))
-autocmd FileType cpp nnoremap <leader>fw :CompileAndRunCpp<CR>
-
-command! -nargs=0 RunPy call TermWrapper(printf('python3 %s', expand('%')))
-autocmd FileType python nnoremap <leader>fw :RunPy<CR>
-
-autocmd FileType html nnoremap <leader>fw :term npx parcel %<CR>
-
-command! -nargs=0 RunGo call TermWrapper(printf('go run %s', expand('%')))
-autocmd FileType go nnoremap <leader>fw :RunGo<CR>
-
-" options
-" choose between 'vertical' and 'horizontal' for how the terminal window is split
-" (default is vertical)
-let g:split_term_style = 'vertical'
-" set splitbelow
-
-" add a custom command to resize the terminal window to your preference
-" (default is to split the screen equally)
-" let g:split_term_resize_cmd = 'resize 6'
-" for horizontal term_style
-let g:split_term_resize_cmd = 'vertical resize 40'
-" (or let g:split_term_resize_cmd = 'vertical resize 40')
+    augroup run_file
+        autocmd BufEnter *.cpp let @g=":w\<CR> :!g++ -std=c++17 \"%\"\<CR>:40vsp | term ./a.out\<CR>i"
+        autocmd BufEnter *.py let @g=":w\<CR>:40vsp | term python3 \"%\"\<CR>i"
+        autocmd BufEnter *.go let @g=":w\<CR>:40vsp | term go run \"%\"\<CR>i"
+        autocmd BufEnter *.html let @g=":w\<CR>:40vsp | term npx parcel \"%\"\<CR>i"
+    augroup end
 ]]
 
 return M
