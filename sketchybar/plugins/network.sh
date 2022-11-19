f11f299e1a1e566e750cf8c1c12e0a3ec9ad8fac
Rishabh Verma <10rishabhv@gmail.com>
2022-11-19 22:06:36 +0530

    hide network speed when not connected

--- sketchybar/plugins/network.sh
+++ sketchybar/plugins/network.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+airport=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
+AIRPORT=$(echo "$airport" | awk 'NR==1 {print $2}')
+LABEL=$(echo "$airport" | awk 'NR==13 {print $2 $3 $4 $5}')
 UPDOWN=$(ifstat -i "en0" -b 0.1 1 | tail -n1)
 DOWN=$(echo $UPDOWN | awk "{ print \$1 }" | cut -f1 -d ".")
 UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f1 -d ".")
@@ -7,20 +10,24 @@ DOWN_SPEED=$((DOWN/8))
 UP_SPEED=$((UP/8))
 
 SPEED=""
-if [ "$UP_SPEED" -gt "$DOWN_SPEED" ]; then
-  sketchybar -m --set network icon=
-  if [ "$UP_SPEED" -gt "999" ]; then
-    SPEED=$(echo $UP_SPEED | awk '{ printf "%.1f MB/s", $1 / 1000}')
-  else
-    SPEED=$(echo $UP_SPEED | awk '{ printf "%.1f KB/s", $1}')
-  fi
+if [ $AIRPORT = "Off" || -z $LABEL ]; then
+    sketchybar -m --set network drawing=off
+elif [ "$UP_SPEED" -gt "$DOWN_SPEED" ]; then
+    sketchybar -m --set network icon=
+    sketchybar -m --set network drawing=on
+    if [ "$UP_SPEED" -gt "999" ]; then
+        SPEED=$(echo $UP_SPEED | awk '{ printf "%.1f MB/s", $1 / 1000}')
+    else
+        SPEED=$(echo $UP_SPEED | awk '{ printf "%.1f KB/s", $1}')
+    fi
 else
-  sketchybar -m --set network icon=
-  if [ "$DOWN_SPEED" -gt "999" ]; then
-    SPEED=$(echo $DOWN_SPEED | awk '{ printf "%.1f MB/s", $1 / 1000}')
-  else
-    SPEED=$(echo $DOWN_SPEED | awk '{ printf "%.1f KB/s", $1}')
-  fi
+    sketchybar -m --set network icon=
+    sketchybar -m --set network drawing=on
+    if [ "$DOWN_SPEED" -gt "999" ]; then
+        SPEED=$(echo $DOWN_SPEED | awk '{ printf "%.1f MB/s", $1 / 1000}')
+    else
+        SPEED=$(echo $DOWN_SPEED | awk '{ printf "%.1f KB/s", $1}')
+    fi
 fi
 sketchybar -m --set network label="$SPEED"
 
