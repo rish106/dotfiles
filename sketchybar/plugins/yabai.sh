fab7185824109b1e71935ba28271b0f38118025d
Rishabh Verma <10rishabhv@gmail.com>
2022-11-20 16:16:42 +0530

    change border size, colors of sketchybar and yabai

--- sketchybar/plugins/yabai.sh
+++ sketchybar/plugins/yabai.sh
@@ -10,16 +10,16 @@ update() {
   args=()
   if [[ $CURRENT -gt 0 ]]; then
     LAST=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
-    args+=(--set $NAME icon=$YABAI_STACK icon.color=$RED label.drawing=on label=$(printf "[%s/%s]" "$CURRENT" "$LAST"))
-    yabai -m config active_window_border_color $RED > /dev/null 2>&1 &
+    args+=(--set $NAME icon=$YABAI_STACK icon.color=$GREEN label.drawing=on label=$(printf "[%s/%s]" "$CURRENT" "$LAST"))
+    yabai -m config active_window_border_color $GREEN > /dev/null 2>&1 &
 
   else 
     args+=(--set $NAME label.drawing=off)
     case "$(echo "$WINDOW" | jq '.["is-floating"]')" in
       "false")
         if [ "$(echo "$WINDOW" | jq '.["has-fullscreen-zoom"]')" = "true" ]; then
-          args+=(--set $NAME icon=$YABAI_FULLSCREEN_ZOOM icon.color=$GREEN)
-          yabai -m config active_window_border_color $GREEN > /dev/null 2>&1 &
+          args+=(--set $NAME icon=$YABAI_FULLSCREEN_ZOOM icon.color=$YELLOW)
+          yabai -m config active_window_border_color $YELLOW > /dev/null 2>&1 &
         elif [ "$(echo "$WINDOW" | jq '.["has-parent-zoom"]')" = "true" ]; then
           args+=(--set $NAME icon=$YABAI_PARENT_ZOOM icon.color=$BLUE)
           yabai -m config active_window_border_color $BLUE > /dev/null 2>&1 &
