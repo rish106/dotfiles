0abe9ecdb777530d58251cded9195903d8d18c1c
Rishabh Verma <10rishabhv@gmail.com>
2022-12-22 10:49:21 +0530

    add typescript lsp

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -9,4 +9,12 @@ return {
 	["NvChad/nvterm"] = false,
 	["folke/which-key.nvim"] = false,
 
+    -- change config
+    ["neovim/nvim-lspconfig"] = {
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.plugins.lspconfig"
+        end,
+    },
+
 }
---
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -0,0 +1,12 @@
+local lspconfig = require "lspconfig"
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local servers = { "tsserver" }
+
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+end
