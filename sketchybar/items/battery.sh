eecd6955cf2f9d0f8d5dac2d6d6b5ccdb2053828
Rishabh Verma <10rishabhv@gmail.com>
2022-12-31 18:41:26 +0530

    fix indentation in sketchybar files

--- sketchybar/items/battery.sh
+++ sketchybar/items/battery.sh
@@ -1,9 +1,9 @@
-sketchybar  --add       item        battery     right						\
-            --set       battery     script="$PLUGIN_DIR/battery.sh"	        \
-                                    update_freq=5						    \
-                                    label.font="$FONT:Light:20.0"	        \
-                                    icon.font="$FONT:Semibold:12.0"	        \
-                                    label.color=$WHITE				        \
-                                    icon.color=$WHITE				        \
+sketchybar  --add       item        battery     right                       \
+            --set       battery     script="$PLUGIN_DIR/battery.sh"         \
+                                    update_freq=5                           \
+                                    label.font="$FONT:Light:20.0"           \
+                                    icon.font="$FONT:Semibold:12.0"         \
+                                    label.color=$WHITE                      \
+                                    icon.color=$WHITE                       \
                                     label.padding_right=8                   \
             --subscribe battery     system_woke
--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -6,12 +6,12 @@ SPACE_CLICK_SCRIPT="yabai -m space --focus \$SID 2>/dev/null"
 for i in "${!SPACE_ICONS[@]}"
 do
   sid=$(($i+1))
-  sketchybar --add space      space.$sid left                   \
-             --set space.$sid associated_space=$sid             \
-                              icon=${SPACE_ICONS[i]}            \
-                              icon.color=$GREY					\
-                              icon.padding_right=10             \
-                              icon.highlight_color=$CYAN        \
-                              label.drawing=off                 \
+  sketchybar --add space      space.$sid left                       \
+             --set space.$sid associated_space=$sid                 \
+                              icon=${SPACE_ICONS[i]}                \
+                              icon.color=$GREY                      \
+                              icon.padding_right=10                 \
+                              icon.highlight_color=$CYAN            \
+                              label.drawing=off                     \
                               click_script="$SPACE_CLICK_SCRIPT"
 done
