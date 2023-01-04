7950b69628df5ba399b784b2abc4df03d946d313
Rishabh Verma <10rishabhv@gmail.com>
2023-01-04 14:11:58 +0530

    add mediainfo preview for image, video and audio files

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -5,6 +5,7 @@ case "$1" in
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
+    *.png | *.jpg | *.jpeg | *.svg | *.gif | *.webp | *.mp4 | *.mkv | *.mov | *.m4v | *.avi | *.flc | *.webm | *.wmv | *.mp3 | *.flac | *.m4a | *.wav | *.opus | *.mpc | *.aac) mediainfo "$1";;
     *.pdf) pdftotext -l 10 -nopgbrk -q -- "$1" -;;
     *) bat --plain --paging=never --theme=DarkNeon --color=always "$1";;
 esac
