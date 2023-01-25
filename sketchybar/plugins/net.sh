14c4eded97445db4f149294ce9057289ea8c5f9c
Rishabh Verma <10rishabhv@gmail.com>
2023-01-25 14:09:12 +0530

    [sketchybar] remove warnings in plugin scripts

--- sketchybar/plugins/net.sh
+++ sketchybar/plugins/net.sh
@@ -7,7 +7,7 @@ airport=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Curren
 AIRPORT=$(echo "$airport" | awk 'NR==1 {print $2}')
 LABEL=$(echo "$airport" | grep -o "SSID: .*" | sed 's/^SSID: //')
 
-if [ $AIRPORT = "Off" ]; then
+if [ "$AIRPORT" = "Off" ]; then
     sketchybar -m --set net icon=$WIFI_OFF              \
                             icon.color=$WHITE           \
                             label.padding_right=2
