ac2b4e976e26d454193036df669edd61fecfe0ff
Rishabh Verma <10rishabhv@gmail.com>
2023-06-16 05:36:21 +0530

    [lf] add cleaner script, kitty image preview

---
+++ scripts/cleaner.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+kitty +kitten icat --clear --stdin no --transfer-mode memory < /dev/null > /dev/tty
--- scripts/preview.sh
+++ scripts/preview.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env sh
 
+FILE="$1"
+PV_WIDTH="$2"
+PV_HEIGHT="$3"
+HORIZONTAL_POS="${4}"
+VERTICAL_POS="${5}"
+
+image_preview() {
+    mediainfo "$FILE"
+    # kitty +kitten icat --transfer-mode file --stdin no --place "${PV_WIDTH}x${PV_HEIGHT}@${HORIZONTAL_POS}x${VERTICAL_POS}" "$FILE" < /dev/null > /dev/tty
+}
+
 case "$1" in
     *.tar*) tar tf "$1";;
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
-    *.png | *.jpg | *.jpeg | *.svg | *.gif | *.webp | *.mp4 | *.mkv | *.mov | *.m4v | *.avi | *.flc | *.webm | *.wmv | *.mp3 | *.flac | *.m4a | *.wav | *.opus | *.mpc | *.aac) mediainfo "$1";;
+    *.png | *.jpg | *.jpeg | *.svg | *.gif | *.webp) image_preview;;
+    *.mp4 | *.mkv | *.mov | *.m4v | *.avi | *.flc | *.webm | *.wmv | *.mp3 | *.flac | *.m4a | *.wav | *.opus | *.mpc | *.aac) mediainfo "$1";;
     *.pdf) pdftotext -l 10 -nopgbrk -q -- "$1" -;;
     *) bat --plain "$1";;
 esac
