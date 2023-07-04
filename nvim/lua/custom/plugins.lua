a7e8a523e4e325def8b2b181125413c92664dc22
Rishabh Verma <10rishabhv@gmail.com>
2023-07-04 23:39:42 +0530

    [nvim] add nvim-surround plugin

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -70,6 +70,16 @@ local plugins = {
     opts = {},
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {},
+    config = function (_, opts)
+      require("nvim-surround").setup(opts)
+    end
+  },
+
   -- To make a plugin not be loaded
   {
     "NvChad/nvterm",
