1ae450cb11118a13e80b2e587c2b0c7af399f6a9
Rishabh Verma <10rishabhv@gmail.com>
2023-07-10 23:24:58 +0530

    [lf] change preview script format to filetypes

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -7,18 +7,26 @@ HORIZONTAL_POS="${4}"
 VERTICAL_POS="${5}"
 
 image_preview() {
-    # mediainfo "$FILE"
     kitty +kitten icat --transfer-mode file --stdin no --place "${PV_WIDTH}x${PV_HEIGHT}@${HORIZONTAL_POS}x${VERTICAL_POS}" "$FILE" < /dev/null > /dev/tty
     exit 1
 }
 
-case "$1" in
-    *.tar*) tar tf "$1";;
-    *.zip) unzip -l "$1";;
-    *.rar) unrar l "$1";;
-    *.7z) 7z l "$1";;
-    *.png | *.jpg | *.jpeg | *.svg | *.gif | *.webp) image_preview;;
-    *.mp4 | *.mkv | *.mov | *.m4v | *.avi | *.flc | *.webm | *.wmv | *.mp3 | *.flac | *.m4a | *.wav | *.opus | *.mpc | *.aac) mediainfo "$1";;
-    *.pdf) pdftotext -l 10 -nopgbrk -q -- "$1" -;;
-    *) bat --plain "$1";;
+video_preview() {
+    mediainfo "$FILE"
+    # timg -p kitty --center --fit-width -t15 "$FILE"
+    # exit 1
+}
+
+MIME_TYPE="$(file --dereference --brief --mime-type "$FILE")"
+
+case "$MIME_TYPE" in
+    application/x-tar) tar tf "$FILE";;
+    application/zip) unizp -l "$FILE";;
+    application/x-rar) unrar l "$FILE";;
+    application/x-7z-compressed) 7z l "$FILE";;
+    image/*) image_preview;;
+    video/*) video_preview;;
+    audio/*) mediainfo "$FILE";;
+    application/pdf) pdftotext -l 10 -nopgbrk -q -- "$FILE" -;;
+    *) bat --plain "$FILE";;
 esac
