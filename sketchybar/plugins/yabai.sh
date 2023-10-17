c948a9b57415b74b9ce575fc1a95d649a8bc245a
Rishabh Verma <10rishabhv@gmail.com>
2023-10-17 20:54:25 +0530

    [sketchybar] change margins, add variables for window border colors

--- sketchybar/plugins/yabai.sh
+++ sketchybar/plugins/yabai.sh
@@ -10,27 +10,27 @@ update() {
     args=()
     if [[ $CURRENT -gt 0 ]]; then
         LAST=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
-        args+=(--set $NAME icon=$YABAI_STACK icon.color=$GREEN label.drawing=on label=$(printf "[%s/%s]" "$CURRENT" "$LAST"))
-        yabai -m config active_window_border_color $GREEN > /dev/null 2>&1 &
+        args+=(--set $NAME icon=$YABAI_STACK icon.color=$STACKED_WINDOW_COLOR label.drawing=on label=$(printf "[%s/%s]" "$CURRENT" "$LAST"))
+        yabai -m config active_window_border_color $STACKED_WINDOW_COLOR > /dev/null 2>&1 &
 
     else 
         args+=(--set $NAME label.drawing=off)
         case "$(echo "$WINDOW" | jq '.["is-floating"]')" in
             "false")
                 if [ "$(echo "$WINDOW" | jq '.["has-fullscreen-zoom"]')" = "true" ]; then
-                    args+=(--set $NAME icon=$YABAI_FULLSCREEN_ZOOM icon.color=$YELLOW)
-                    yabai -m config active_window_border_color $YELLOW > /dev/null 2>&1 &
+                    args+=(--set $NAME icon=$YABAI_FULLSCREEN_ZOOM icon.color=$FULLSCREEN_WINDOW_COLOR)
+                    yabai -m config active_window_border_color $FULLSCREEN_WINDOW_COLOR > /dev/null 2>&1 &
                 elif [ "$(echo "$WINDOW" | jq '.["has-parent-zoom"]')" = "true" ]; then
-                    args+=(--set $NAME icon=$YABAI_PARENT_ZOOM icon.color=$BLUE)
-                    yabai -m config active_window_border_color $BLUE > /dev/null 2>&1 &
+                    args+=(--set $NAME icon=$YABAI_PARENT_ZOOM icon.color=$PARENT_ZOOM_WINDOW_COLOR)
+                    yabai -m config active_window_border_color $PARENT_ZOOM_WINDOW_COLOR > /dev/null 2>&1 &
                 else
-                    args+=(--set $NAME icon=$YABAI_GRID icon.color=$WHITE)
-                    yabai -m config active_window_border_color $WHITE > /dev/null 2>&1 &
+                    args+=(--set $NAME icon=$YABAI_GRID icon.color=$ACTIVE_WINDOW_COLOR)
+                    yabai -m config active_window_border_color $ACTIVE_WINDOW_COLOR > /dev/null 2>&1 &
                 fi
                 ;;
             "true")
-                args+=(--set $NAME icon=$YABAI_FLOAT icon.color=$MAGENTA)
-                yabai -m config active_window_border_color $MAGENTA > /dev/null 2>&1 &
+                args+=(--set $NAME icon=$YABAI_FLOAT icon.color=$FLOATING_WINDOW_COLOR)
+                yabai -m config active_window_border_color $FLOATING_WINDOW_COLOR > /dev/null 2>&1 &
                 ;;
         esac
     fi
