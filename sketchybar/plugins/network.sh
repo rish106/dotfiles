376c357e71283793e97abaddf8ab83f1ce915b92
Rishabh Verma <10rishabhv@gmail.com>
2023-10-29 22:59:27 +0530

    [sketchybar] refactor network plugin, fix padding

--- sketchybar/plugins/network.sh
+++ sketchybar/plugins/network.sh
@@ -5,29 +5,26 @@ source "$HOME/.config/sketchybar/icons.sh"
 airport=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
 AIRPORT=$(echo "$airport" | awk 'NR==1 {print $2}')
 LABEL=$(echo "$airport" | grep -o "SSID: .*" | sed 's/^SSID: //')
-UPDOWN=$(ifstat -i "en0" 0.1 1 | tail -n 1)
-DOWN=$(echo $UPDOWN | awk "{ print \$1 }" | cut -f 1 -d ".")
-UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f 1 -d ".")
-DOWN_SPEED=$((DOWN/1))
-UP_SPEED=$((UP/1))
-SPEED=""
+UPDOWN="$(ifstat -i "en0" 0.1 1 | tail -n 1)"
+DOWN_SPEED=$(($(echo "$UPDOWN" | awk "{ print \$1 }" | cut -f 1 -d ".")))
+UP_SPEED=$(($(echo "$UPDOWN" | awk "{ print \$2 }" | cut -f 1 -d ".")))
 
-if [ "$AIRPORT" = "Off" ] || [ -z "$LABEL" ]; then
+if [ "$AIRPORT" -eq "Off" ] || [ -z "$LABEL" ]; then
     sketchybar -m --set network icon.drawing=off
-elif [ "$UP_SPEED" -gt "$DOWN_SPEED" ]; then
+elif [ $UP_SPEED -gt $DOWN_SPEED ]; then
     sketchybar -m --set network icon.drawing=on icon=$UPLOAD
-    if [ "$UP_SPEED" -gt "999" ]; then
+    if [ $UP_SPEED -gt 999 ]; then
         SPEED=$(echo $UP_SPEED | awk '{ printf "%.1f MB/s", $1 / 1000}')
     else
         SPEED=$(echo $UP_SPEED | awk '{ printf "%.1f KB/s", $1}')
     fi
 else
     sketchybar -m --set network icon.drawing=on icon=$DOWNLOAD
-    if [ "$DOWN_SPEED" -gt "999" ]; then
+    if [ $DOWN_SPEED -gt 999 ]; then
         SPEED=$(echo $DOWN_SPEED | awk '{ printf "%.1f MB/s", $1 / 1000}')
     else
         SPEED=$(echo $DOWN_SPEED | awk '{ printf "%.1f KB/s", $1}')
     fi
 fi
-sketchybar -m --set network label="$SPEED"
 
+sketchybar -m --set network label="$SPEED"
